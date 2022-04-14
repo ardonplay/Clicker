8486a2d2872e8afbccc7e353fd1b3303653b86c1
ardonplay <ardonplay@gmail.com>
2022-04-15 00:19:27 +0300

    da

--- Clicker WatchKit Extension/ContentView.swift
+++ Clicker WatchKit Extension/ContentView.swift
@@ -1,16 +1,38 @@
 //
 //  ContentView.swift
-//  Clicker WatchKit Extension
+//  test
 //
 //  Created by Vladimir Moshchuk on 14.04.22.
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var funFact = "0"
+    @State private var index = 0
+    @State private var Congratulations = ""
+    @State private var n = 1
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+       
+        Button("ТЫК"){
+        index = index + 1
+        funFact = String(index)
+        if (index % (10*n*n) == 0 && index != 0){
+            Congratulations = "Yeah!"
+            n+=1
+        }
+            else{
+                Congratulations = ""
+            }
+        }
+        .position(.init(x: 80, y: 50))
+        Button("Очистка"){
+          index = 0
+            n = 1
+            funFact = String(index)
+        }
+        .position(.init(x: 80, y: 100))
     }
 }
 
--- Clicker/ContentView.swift
+++ Clicker/ContentView.swift
@@ -1,16 +1,47 @@
 //
 //  ContentView.swift
-//  Clicker
+//  test
 //
 //  Created by Vladimir Moshchuk on 14.04.22.
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var funFact = "0"
+    @State private var index = 0
+    @State private var Congratulations = ""
+    @State private var n = 1
     var body: some View {
-        Text("Hello, world!")
+        Text (Congratulations)
             .padding()
+            .font(.largeTitle)
+            .frame(minHeight: 100)
+        Text(funFact)
+            .padding()
+            .font(.largeTitle)
+            .frame(minHeight: 100)
+        Button("ТЫК"){
+        index = index + 1
+        funFact = String(index)
+        if (index % (10*n*n) == 0 && index != 0){
+            Congratulations = "Yeah!"
+            n+=1
+        }
+            else{
+                Congratulations = ""
+            }
+        }
+        .font(.title)
+        .padding(20)
+        
+       
+        Button("Очистка"){
+          index = 0
+            n = 1
+            funFact = String(index)
+        }
     }
 }
 
